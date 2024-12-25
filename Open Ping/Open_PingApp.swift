e34250c3e64f806af4a778d4962dc846a105d95e
merlos <merlos@users.noreply.github.com>
2024-12-25 16:38:23 +0300

    Initial Commit

---
+++ Open Ping/Open_PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_PingApp.swift
+//  Open Ping
+//
+//  Created by Merlos on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Open_PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Open PingTests/Open_PingTests.swift
@@ -0,0 +1,16 @@
+//
+//  Open_PingTests.swift
+//  Open PingTests
+//
+//  Created by Merlos on 12/25/24.
+//
+
+import Testing
+
+struct Open_PingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
